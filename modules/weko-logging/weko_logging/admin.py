3acabf14f4fa32774b177b841708c80d1dbb5a81
ivis-kikuchi <shunya.kikuchi@ivis.co.jp>
2026-09-24 19:00:51 +0900

    fix: export log

--- modules/weko-logging/weko_logging/admin.py
+++ modules/weko-logging/weko_logging/admin.py
@@ -17,6 +17,7 @@ from invenio_files_rest.models import FileInstance
 from weko_logging.utils import UserActivityLogUtils
 from weko_logging.tasks import export_all_user_activity_logs
 from weko_search_ui.tasks import check_celery_is_run
+from weko_redis.redis import RedisConnection
 
 class ExportLogAdminView(BaseView):
     """Export user activity log view."""
@@ -42,14 +43,17 @@ class ExportLogAdminView(BaseView):
         # get current task running
         task_status = UserActivityLogUtils.get_export_task_status()
         if task_status and task_status.get('task_id'):
-            task = export_all_user_activity_logs.AsyncResult(
-                task_status.get('task_id')
-            )
-            if task.state == states.PENDING or task.state == states.STARTED:
-                return jsonify({
-                    'code': 409,
-                    'data': {'message': _('Export task is running.')}
-                })
+            is_deleted = self.is_celery_task_meta_missing(task_status.get('task_id'))
+            if not is_deleted:
+                task = export_all_user_activity_logs.AsyncResult(
+                    task_status.get('task_id')
+                )
+
+                if task.state == states.PENDING or task.state == states.STARTED:
+                    return jsonify({
+                        'code': 409,
+                        'data': {'message': _('Export task is running.')}
+                    })
 
         UserActivityLogUtils.clear_export_status()
         task = export_all_user_activity_logs.delay()
@@ -131,6 +135,24 @@ class ExportLogAdminView(BaseView):
         else:
             abort(404)
 
+    def is_celery_task_meta_missing(self, task_id):
+        """
+        Checks if the Celery task meta information for the given task_id is missing.
+
+        Args:
+            task_id (str): Celery task ID.
+
+        Returns:
+            bool: True if deleted, otherwise False.
+        """
+        is_deleted = False
+        meta_task_id = "celery-task-meta-{}".format(task_id)
+        redis_connection_celery = RedisConnection()
+        datastore_celery = redis_connection_celery.connection(db=current_app.config['CELERY_RESULT_BACKEND_DB_NO'], kv = True)
+        if not datastore_celery.redis.exists(meta_task_id):
+            is_deleted = True
+        return is_deleted
+
 log_export_admin_view = {
     "view_class": ExportLogAdminView,
     "kwargs": {
@@ -144,3 +166,4 @@ log_export_admin_view = {
 __all__ = (
     'log_export_admin_view',
 )
+
